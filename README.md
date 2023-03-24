10e45bd70f8fc0dd319dcc037fef0af10024e8f5
Anastasia <maidacenco100@gmail.com>
2023-03-24 11:41:59 +0200

    formatting

---
+++ README.md
@@ -0,0 +1,7 @@
+# aiogram telegram Bot_Payments
+https://integration.unlimint.com/#createPayment
+
+UNLIMIT - система предоставляющая бесплатные тестовые карточки
+
+https://core.telegram.org/bots/payments#supported-currencies
+
---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = '5955318245:AAGzlk5zJbyvY07QMeLVJ2mNoMLOxWjBLtk'
+PAYMENTS_TOKEN = '5322214758:TEST:01ed1fa3-6061-4aee-b41b-5a34ae33f349'
+
+item_url = 'https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1468&q=80'
